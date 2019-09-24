639f1e8417d035ec82369e922864b330938cfd21
Vladimir_Valitsky <vladimir.valitsky@vrpconsulting.com>
2019-09-24 14:48:10 +0300

    add assign test

--- src/aura/warehouses/warehousesController.js
+++ src/aura/warehouses/warehousesController.js
@@ -4,9 +4,9 @@
 ({
     doInit: function(component, event, helper) {
             helper.getRecords(component);
-        },
+    },
+
     changedWarehouse: function(component, event, helper) {
-            console.log('222222');
             var selected = Array.prototype.filter.call(component.find("selectedWarehouseId").getElement().options,
                     function(option) {
                         return option.selected;
@@ -16,21 +16,17 @@
                         return option.value;
                     }
                 );
-            console.log('selected>>>****' + selected);
-            component.set('v.selectWar', selected);
-             },
+            component.set('v.selectWar', selected.valueOf());
+    },
+
     displayDevice: function(component, event, helper) {
-        console.log('111111');
         var selectedWarehouse = event.getSource().get('v.value');
-        console.log('selectedWarehouse>>>' + selectedWarehouse);
         helper.displayDeviceByWarehouse(component, selectedWarehouse);
-         },
+    },
+
     assignDevice: function(component, event, helper) {
-        console.log('33333');
             var selectedDeviceParameter = event.getSource().get('v.value');
             var selectedWarehouseParameter = component.get('v.selectWar');
-            console.log('selectedDeviceParameter>>>' + selectedDeviceParameter);
-            console.log('selectedWarehouseParameter>>>' + selectedWarehouseParameter);
             helper.assignDeviceHelper(component, selectedDeviceParameter, selectedWarehouseParameter);
-     },
+    },
 })
--- src/aura/warehouses/warehousesHelper.js
+++ src/aura/warehouses/warehousesHelper.js
@@ -7,7 +7,6 @@
         action.setCallback(this, $A.getCallback(function (response) {
             var state = response.getState();
             if(state === "SUCCESS"){
-                console.log(response.getReturnValue());
                 component.set('v.warehousesNameList', response.getReturnValue());
             } else if (state === "ERROR") {
                 var errors = response.getError();
@@ -16,33 +15,27 @@
         }));
         $A.enqueueAction(action);
         },
+
         displayDeviceByWarehouse : function(component, selectedWarehouseParam){
             var action = component.get('c.devicesByWarehouse');
             action.setParams({selectedWarehouse : selectedWarehouseParam});
             action.setCallback(this, function(result){
-                console.log('SUCCESS');
-                console.log('result---- ' + result.getReturnValue());
                 var custs = [];
                 var conts = result.getReturnValue();
-                console.log('conts---- ' + conts);
                 for ( var key in conts ) {
                 custs.push({value:conts[key], key:key});
                 }
-
-                console.log('custs' + custs);
                 component.set("v.devices", custs);
             });
          $A.enqueueAction(action);
         },
+
         assignDeviceHelper : function(component, selectedDeviceParam, selectedWarehouseParam){
              var action = component.get('c.assignDeviceOnWarehouse');
-             action.setParams({selectedDeviceParameter : selectedDeviceParam, selectedWarehouseParameter : selectedWarehouseParam});
-             console.log('selectedDeviceParam///////////' + selectedDeviceParam);
-             console.log('selectedWarehouseParam+++++++++' + selectedWarehouseParam);
+             action.setParams({'selectedDeviceParameter' : selectedDeviceParam,
+             'selectedWarehouseParameter' : selectedWarehouseParam.valueOf()});
              action.setCallback(this, function(result){
-                 console.log('state: ' + state);
              if(state === "SUCCESS"){
-                 console.log(response.getReturnValue());
                  component.set('v.resultAssign', response.getReturnValue());
                  } else if (state === "ERROR") {
                      var errors = response.getError();
